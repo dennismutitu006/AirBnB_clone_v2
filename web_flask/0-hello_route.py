189057e58f69d8ffbc34bad7dd27b0ca8aaf4dfe
dennismutitu <dennismutitu06@gmail.com>
2024-03-25 01:23:30 -0700

    server up and running

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,5 +10,5 @@ def hello_hbnb():
     """Print webpage content"""
     return 'Hello HBNB!'
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
