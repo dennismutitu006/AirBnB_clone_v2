56295f22267480614ca676622d556ac19705d44c
dennismutitu <dennismutitu06@gmail.com>
2024-03-25 01:40:11 -0700

    added another route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This script will start a flask web app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Print web"""
+    return 'Hello HBNB!'
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display"""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
